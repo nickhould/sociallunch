cbf9242a25b85e5acc9fc2d8ee0be8edd2ca7248
Jean-Nicholas <nick.hould@gmail.com>
2012-06-09 11:15:06 -0400

    Initial commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sociallunch::Application.config.secret_token = 'd4f03df6ea2b1e0eaec3a221a5947f45dfec5fc79f29686509f0b577cfa2eaed93ef87bf68fbe6b6ec2c6b24858673d14d95e81f788d7a70dc4920505d09e68e'
