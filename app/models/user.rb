22ac8df0e578e7750ea5a3fd704cae9a678208cf
Charles Lalonde <charles.lalonde@okam.ca>
2012-06-09 11:21:12 -0400

    Ajout du model user et mise en place de Koala

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+  attr_accessible :facebook_id
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Sociallunch::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
